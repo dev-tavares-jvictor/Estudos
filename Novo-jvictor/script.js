4ecaee9ee79e49866a9bfc33efcaa46a06ecb60e
dev-tavares-jvictor <joao91717@gmail.com>
2024-06-05 22:36:53 -0300

    subindo site git

--- Novo-jvictor/script.js
+++ Novo-jvictor/script.js
@@ -1,7 +1,1 @@
-const bru = document.querySelector(".bru");
-const navMenu = document.querySelector(".nav-menu");
 
-bru.addEventListener("click", () => {
-    bru.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
