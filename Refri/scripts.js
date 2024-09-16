58d6af2849489c1cdda1bcecf61cdf530b05937c
dev-tavares-jvictor <joao91717@gmail.com>
2024-09-16 13:48:11 -0300

    subindo

---
+++ Refri/scripts.js
@@ -0,0 +1,15 @@
+let list = document.querySelectorAll('.item')
+let next = document.getElementById('.next')
+let prev = document.getElementById('.prev')
+
+
+
+//count = 3
+// 0, 1, 2
+
+let count = list.length
+let active = 0
+
+next.onclick = () => {
+    console,log("Abacate 123")
+}
